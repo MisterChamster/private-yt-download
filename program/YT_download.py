fb9ec5ab5cea21487a4982e978ef0893edb624e4
MisterChamster <s.duda03@gmail.com>
2025-06-10 10:53:43 +0200

    Restructuring 5

--- program/YT_download.py
+++ program/YT_download.py
@@ -18,99 +18,14 @@ from functions import (CharPolice,
                        IsInternetAvailable,
                        ReadDelDuplicates,
                        ReadExtractWriteOrder,
+                       ReadNumbered,
                        ReadNumOfTracks,
                        ReadSaveExtension,
+                       ReadTrimLens,
                        RoundOrExact,
                        ZerosAtBeginning
 )
 
-def ReadNumbered(min_el_index, max_el_index):
-    """
-    Determines numbering in filenames.
-
-    In series of questions, function determines if elements should be numbered, 
-    and if so, in what order and on what number will the numbering start.
-    
-    Args:
-        min_el_index (int): Index of the first element to be numbered.
-        max_el_index (int): Index of the last element to be numbered.
-
-    Returns:
-        list[a, b]: 
-            a (str): Naming order ("asc", "desc", "not").
-            b (int): Number of first downloaded element.
-    """
-    user_input = " "
-
-    while True:
-        user_input = input("Do You want elements to be numbered? (Enter - starting on 1, integer - starting on integer, n - no, c - custom...)\n>>").lower()
-
-        if user_input == "" or user_input == "y":
-            return ["asc", 1]
-        if user_input.isdigit():
-            return ["asc", int(user_input)]
-        if user_input == "n":
-            return ["not", -1]
-        if user_input == "c":
-            if min_el_index != 0:
-                user_input_custom = input("Choose custom numbering option (starting from element's number in playlist [n - normal order, r - reverse order], d - descending from...):\n>>").lower()
-                if user_input_custom == "n":
-                    return ["asc", min_el_index+1]
-            else:
-                user_input_custom = input("Choose custom numbering option (r - reverse order, d - descending from...):\n>>").lower()
-
-            if user_input_custom == "r":
-                return ["desc", max_el_index+1]
-            if user_input_custom == "d":
-                number_of_elements = max_el_index - min_el_index + 1
-                desc_from = input(f"Files will be numbered in reversed order, starting from (not lower than {number_of_elements}):\n>>")
-                if desc_from.isdigit():
-                    desc_from = int(desc_from)
-                    if desc_from >= number_of_elements:
-                        return ["desc", desc_from]
-
-def ReadTrimLens():
-    """
-    Asks user about cutting names of the elements.
-
-    Returns:
-        list[a, b]:
-            a (int): Number of letters cut from the start of filename.
-            b (int): Number of letters cut from the end of filename.
-    """
-    inputSC = " "
-    namecut_list = [0, 0]
-    while inputSC not in ["", "s", "b", "e"]:
-        inputSC = input("Do You want to trim names of all elements? (Enter - no, s - at the start, e - at the end, b - both)\n>>").lower()
-
-    if inputSC == "s":
-        namecut_list[0] = str(input("Input the string or length You want to trim at the start:\n>>"))
-        if namecut_list[0].isdigit():
-            namecut_list[0] = int(namecut_list[0])
-        else:
-            namecut_list[0] = len(namecut_list[0])
-
-    elif inputSC == "e":
-        namecut_list[1] = str(input("Input the string or length You want to trim at the ending:\n>>"))
-        if namecut_list[1].isdigit():
-            namecut_list[1] = int(namecut_list[1])
-        else:
-            namecut_list[1] = len(namecut_list[1])
-
-    elif inputSC == "b":
-        namecut_list[0] = str(input("Input the string or length You want to trim at the start:\n>>"))
-        if namecut_list[0].isdigit():
-            namecut_list[0] = int(namecut_list[0])
-        else:
-            namecut_list[0] = len(namecut_list[0])
-
-        namecut_list[1] = str(input("Input the string or length You want to trim at the ending:\n>>"))
-        if namecut_list[1].isdigit():
-            namecut_list[1] = int(namecut_list[1])
-        else:
-            namecut_list[1] = len(namecut_list[1])
-
-    return namecut_list
 
 def NameYourFile(OGtitle, title_number, namecut_list):
     """

e61ca7d541736d06665fe1e37d6cb61d14cf744c
Mr Chamster <s.duda03@gmail.com>
2025-07-04 23:24:59 +0200

    Reading modules file division decl

---
+++ src/modules_reading_playlist.py
@@ -0,0 +1,6 @@
+from yt_dlp import YoutubeDL
+from math import ceil
+from datetime import date
+from time import localtime, strftime
+from socket import create_connection
+from os import chdir, mkdir, path, listdir

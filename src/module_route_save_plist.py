0a58fb0b668cc133f835d0bc4b9c988aa2509fec
Mr Chamster <s.duda03@gmail.com>
2025-07-06 19:36:26 +0200

    Routes file division

--- src/module_route_save_plist.py
+++ src/module_route_save_plist.py
@@ -14,54 +14,6 @@ from .module_askers_playlist import (ask_del_duplicates,
 
 
 
-def save_single(url):
-    """
-    Downloads element from given URL
-
-    Extracts video title from URL, checks if it's correct, adjusts YoutubeDL 
-    object parameters and downloads.
-
-    Args:
-        url (str) - correct URL to youtube video
-    """
-    ydl_opts = get_ydl_options()
-    ydl_getdata = {'quiet': True,
-                   'extract_flat': True,
-                   'force_generic_extractor': True}
-    desktop_path = path.join(path.expanduser("~"), "Desktop")
-
-    try:
-        with YoutubeDL(ydl_getdata) as ydl:
-            og_title = ydl.extract_info(url, download=False)["title"]
-    except:
-        if not is_internet_available():
-            print("Internet connection failed.\n\n")
-            return
-        else:
-            print("Something went wrong")
-
-    finalname = char_police(og_title)
-    if finalname == "":
-        finalname = illegal_to_ascii(og_title)
-    i = 1
-    while path.exists(desktop_path + "/" + finalname):
-        finalname += "_d"*i
-        i += 1
-
-    ydl_opts["outtmpl"] = finalname
-    ydl_opts["paths"] = {"home": desktop_path}
-    try:
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        print("\n" + finalname + " has been successfully downloaded")
-    except:
-        if not is_internet_available():
-            print("Internet connection failed.\n\n")
-            return
-        else:
-            print("Something went wrong")
-
-
 def save_plist(plist_url): 
     """
     Downloads elements from a youtube playlist.
---
+++ src/module_route_save_single.py
@@ -0,0 +1,55 @@
+from yt_dlp import YoutubeDL
+from os import path
+from .module_utils import (char_police,
+                           illegal_to_ascii,
+                           is_internet_available,
+                           get_ydl_options)
+
+
+
+def save_single(url):
+    """
+    Downloads element from given URL
+
+    Extracts video title from URL, checks if it's correct, adjusts YoutubeDL 
+    object parameters and downloads.
+
+    Args:
+        url (str) - correct URL to youtube video
+    """
+    ydl_opts = get_ydl_options()
+    ydl_getdata = {'quiet': True,
+                   'extract_flat': True,
+                   'force_generic_extractor': True}
+    desktop_path = path.join(path.expanduser("~"), "Desktop")
+
+    try:
+        with YoutubeDL(ydl_getdata) as ydl:
+            og_title = ydl.extract_info(url, download=False)["title"]
+    except:
+        if not is_internet_available():
+            print("Internet connection failed.\n\n")
+            return
+        else:
+            print("Something went wrong")
+
+    finalname = char_police(og_title)
+    if finalname == "":
+        finalname = illegal_to_ascii(og_title)
+    i = 1
+    while path.exists(desktop_path + "/" + finalname):
+        finalname += "_d"*i
+        i += 1
+
+    ydl_opts["outtmpl"] = finalname
+    ydl_opts["paths"] = {"home": desktop_path}
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("\n" + finalname + " has been successfully downloaded")
+    except:
+        if not is_internet_available():
+            print("Internet connection failed.\n\n")
+            return
+        else:
+            print("Something went wrong")
